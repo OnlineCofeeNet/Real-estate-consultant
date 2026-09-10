3bb86ce136c656aaa2d03e6aeb47c64e25d3490b
OnlineCofeeNet <online_cofee_net@outlook.com>
2026-09-11 00:29:58 +0330

    chore: remove temporary scripts

--- patch_db_cascade.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/db/db.ts', 'utf8');
-
-code = code.replace(
-  "async completeContractTransaction(payload: any) {",
-  "async cascadeDeleteContract(id: number) {\n    const res = await axios.delete(\`/api/contracts/\${id}/cascade\`);\n    return res.data;\n  }\n\n  async completeContractTransaction(payload: any) {"
-);
-
-fs.writeFileSync('src/db/db.ts', code);
